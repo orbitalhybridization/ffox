b099ca9023db0a25e2f50e702a693db3d7983868
orbitalhybridization <orbitalhybridization@gmail.com>
2024-02-18 14:50:29 -0600

    fix favorites

--- setup.sh
+++ setup.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-BASHFILE="$HOME/.bashrc"
-echo "## FFOX DEFAULTS" >> BASHFILE
-echo "export PATH=$PWD:$PATH" >> BASHFILE
-echo "export FFOX_PATH=$PWD" >> BASHFILE
-source BASHFILE
+if [ -z "${FFOX_PATH}" ]; then
+
+
+	BASHFILE="$HOME/.bashrc"
+	echo -e "\n## FFOX DEFAULTS" >> "${BASHFILE}"
+	echo "export PATH=$PWD:$PATH" >> "${BASHFILE}" # add to PATH
+	echo "export FFOX_PATH=$PWD" >> "${BASHFILE}" # create env variable
+	source "${BASHFILE}"
+	sudo chmod u+x "$PWD/ffox" # make executable
+	echo "ffox is set up!"
+
+else
+	echo "ffox appears to already be set up!"
+fi
